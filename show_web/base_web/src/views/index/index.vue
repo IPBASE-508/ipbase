5881497369b8174b541b03d75b982b58ebd3fa86
Legion1998 <43737121+Legion1998@users.noreply.github.com>
2019-10-06 00:27:13 +0800

    first commit首页轮播图完成

--- show_web/base_web/src/views/index/index.vue
+++ show_web/base_web/src/views/index/index.vue
@@ -1,3 +1,38 @@
 <template>
-  <div>This is index page</div>
+  <div id="app" class="body">
+    <VueCarousel
+      :imgs="imgs"
+    :delay="2000"
+    :imgWidth="1960"
+    :imgHeight="747"
+    :dotWidth="20"
+    />
+
+  </div>
 </template>
+
+<script>
+  import VueCarousel from '../../../src/components/indexpicture.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      VueCarousel
+    },
+    data () {
+      return {
+        imgs:[
+          {src: require('../../../src/assets/img/background-head.jpg')},
+          {src: require('../../../src/assets/img/news.png')},
+
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+  .body{
+    background-color:#20487b;
+  }
+</style>
